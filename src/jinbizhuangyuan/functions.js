45719a5442081bb94b902016cbea36c97c16a246
chenxuan <523764720@qq.com>
2020-07-03 15:28:20 +0800

    dev: 初步可以进入猫店

--- src/jinbizhuangyuan/functions.js
+++ src/jinbizhuangyuan/functions.js
@@ -7,6 +7,10 @@ funs.clickAreaByImage = function(imgpath)
 {
     let targetImg = images.read(imgpath);
     let p = images.findImage(captureScreen(), targetImg);
+    if(!p)
+    {
+        throw new Error("no match: " + imgpath)
+    }
     click(p.x, p.y)
     return p
 }
@@ -17,6 +21,7 @@ funs.backToHomePage = function ()
         packageName: "com.taobao.taobao",
         className: "com.taobao.tao.TBMainActivity"
     });
+    waitForActivity("com.taobao.tao.TBMainActivity")
     sleep(1e3)
     let homeBtn = images.read("targetimage/home/homebtn.png");
     let p = images.findImage(captureScreen(), homeBtn);
@@ -33,6 +38,21 @@ funs.backToHomePage = function ()
         click(p.x, p.y)
         sleep(1e3)
     }
+}
+
+funs.toMaochao = function ()
+{
+    funs.backToHomePage()
+    funs.clickAreaByImage("targetimage/miaojing/maochao.png")
+    waitForActivity("com.taobao.browser.BrowserActivity")
+    sleep(3e3)
+}
+
+funs.toMaodian = function ()
+{
+    funs.toMaochao()
+    funs.clickAreaByImage("targetimage/miaojing/miaodianBtn.png")
+    waitForActivity("com.taobao.browser.exbrowser.BrowserUpperActivity")
 }
 
 module.exports = funs
---
+++ src/jinbizhuangyuan/收喵晶.js
@@ -0,0 +1,11 @@
+auto()
+requestScreenCapture();
+let funs = require("functions.js")
+
+console.show()
+
+funs.backToHomePage()
+
+funs.toMaodian()
+
+console.log("inini");
