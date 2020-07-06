e79930c498580af249cbbc8fa93131036b6e1cd9
chenxuan <523764720@qq.com>
2020-07-06 18:26:25 +0800

    dev: 可以偷金币了, 晚点整理一下代码

--- src/jinbizhuangyuan/jinbizhuangyuan.js
+++ src/jinbizhuangyuan/jinbizhuangyuan.js
@@ -1,6 +1,7 @@
 auto()
 // requestScreenCapture();
 let funs = require("functions.js")
+const { backToHomePage } = require("./functions")
 
 console.show()
 
@@ -22,10 +23,50 @@ funs.jiaoshui = function ()
     funs.clickAreaByUIObject(text("浇水").findOne())
 }
 
+funs.loopJiaoshui = function ()
+{
+    while(true)
+    {
+        let jiaoshuiButton = text("可浇水").find()
+        console.log("当前可浇水: " + jiaoshuiButton.length);
+        if(jiaoshuiButton.length == 0)
+        {
+            break
+        }
+        jiaoshuiButton[0].click()
+        sleep(8e3)
+        text("浇水").findOne().click()
+        sleep(3e3)
+        back()
+        sleep(3e3)
+    }
+}
 
-// let jiaoshuiButton = text("可浇水").find()
-// console.log(jiaoshuiButton.length);
+funs.loopToujinbi = function ()
+{
+    while(true)
+    {
+        let toujinbiButton = depth(18).text("偷金币").find()
+        console.log("当前偷金币: " + toujinbiButton.length);
+        if(toujinbiButton.length == 0)
+        {
+            break
+        }
+        toujinbiButton[0].click()
+        sleep(8e3)
+        click(570, 787)
+        sleep(3e3)
+        back()
+        sleep(3e3)
+    }
+}
 
-// funs.clickAreaByUIObject(jiaoshuiButton[0])
 
-back()
+while(true)
+{
+    funs.loopToujinbi()
+    console.log("加载更多好友");
+    text("加载更多好友").findOne().click()
+    sleep(2e3)
+}
+
