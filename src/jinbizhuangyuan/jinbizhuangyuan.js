ed7aff629be18295d08a2dc20972335c5305fb9f
chenxuan <523764720@qq.com>
2020-07-07 15:07:57 +0800

    dev: 现在可以完整跑完金币庄园的好友了

--- src/jinbizhuangyuan/jinbizhuangyuan.js
+++ src/jinbizhuangyuan/jinbizhuangyuan.js
@@ -23,10 +23,16 @@ funs.jiaoshui = function ()
     funs.clickAreaByUIObject(text("浇水").findOne())
 }
 
+funs.loopJiaoshuiCount = 10
+
 funs.loopJiaoshui = function ()
 {
     while(true)
     {
+        if(funs.loopJiaoshuiCount <= 0)
+        {
+            break
+        }
         let jiaoshuiButton = text("可浇水").find()
         console.log("当前可浇水: " + jiaoshuiButton.length);
         if(jiaoshuiButton.length == 0)
@@ -35,7 +41,9 @@ funs.loopJiaoshui = function ()
         }
         jiaoshuiButton[0].click()
         sleep(8e3)
-        text("浇水").findOne().click()
+        funs.jiaoshui()
+        funs.loopJiaoshuiCount--
+        console.log("可浇水次数剩余: " + funs.loopJiaoshuiCount);
         sleep(3e3)
         back()
         sleep(3e3)
@@ -65,8 +73,17 @@ funs.loopToujinbi = function ()
 while(true)
 {
     funs.loopToujinbi()
+    funs.loopJiaoshui()
     console.log("加载更多好友");
-    text("加载更多好友").findOne().click()
+    try
+    {
+        text("加载更多好友").find()[0].click()
+    }
+    catch(e)
+    {
+        console.log("金币庄园好友都搞完了");
+        break
+    }
     sleep(2e3)
 }
 
