f676cf6e551dc82438ffc9f3d8c1fb70c12a4f02
chenxuan <523764720@qq.com>
2020-07-20 20:15:55 +0800

    dev: 原来可以通过悬浮窗来运行ui界面的, 那就可以后台他了

--- src/jinbizhuangyuan/tftest.js
+++ src/jinbizhuangyuan/tftest.js
@@ -1,5 +1,3 @@
-"ui";
-
 auto()
 
 importClass(android.webkit.WebView)
@@ -7,29 +5,41 @@ importClass(android.webkit.WebChromeClient)
 importClass(android.webkit.WebResourceResponse)
 importClass(android.webkit.WebViewClient)
 
-ui.layout(
-    <linear w="*" h="*">
+let w = floaty.window(
+    <linear w="*" h="500">
         <webview id="webview" h="*" w="*" />
     </linear>
-)
+);
+ui.run(function()
+{
+    w.setPosition(0, -80)
+    w.setSize(-1, 120)
+    let webview = w.webview
+    let set = webview.getSettings()
+    set.setAllowFileAccessFromFileURLs(false)
+    set.setAllowUniversalAccessFromFileURLs(false)
+    set.setSupportZoom(false)
+    set.setJavaScriptEnabled(true)
+     
+    // 这里的lesson是开始时创建的文件夹名，请根据自己的情况修改
+    webview.loadUrl('http://baidu.com')
+     
+    var webcc = new JavaAdapter(WebChromeClient, {
+        onJsPrompt: function (webView, url, fnName, defaultValue, jsPromptResult) {
+            toast(fnName + ' ' + defaultValue)
+     
+            // 这段代码是必要的，否则会弹出prompt，阻塞界面。
+            jsPromptResult.confirm()
+            return true
+        }
+    })
+    webview.setWebChromeClient(webcc)
+    
+});
 
-let webview = ui.webview
-let set = webview.getSettings()
-set.setAllowFileAccessFromFileURLs(false)
-set.setAllowUniversalAccessFromFileURLs(false)
-set.setSupportZoom(false)
-set.setJavaScriptEnabled(true)
 
-// 这里的lesson是开始时创建的文件夹名，请根据自己的情况修改
-webview.loadUrl('http://baidu.com')
 
-var webcc = new JavaAdapter(WebChromeClient, {
-    onJsPrompt: function (webView, url, fnName, defaultValue, jsPromptResult) {
-        toast(fnName + ' ' + defaultValue)
-
-        // 这段代码是必要的，否则会弹出prompt，阻塞界面。
-        jsPromptResult.confirm()
-        return true
-    }
-})
-webview.setWebChromeClient(webcc)
+setInterval(function ()
+{
+    console.log(1)
+}, 1e3)
