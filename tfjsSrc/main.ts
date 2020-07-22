cb08b65e6264ca3861cbe53eea704c642df698ee
chenxuan <523764720@qq.com>
2020-07-22 16:15:30 +0800

    dev: 尝试学习使用tfjs

---
+++ tfjsSrc/main.ts
@@ -0,0 +1,18 @@
+
+import * as tf from "@tensorflow/tfjs";
+
+// 训练一个简单模型:
+const model = tf.sequential();
+model.add(tf.layers.dense({units: 100, activation: 'relu', inputShape: [10]}));
+model.add(tf.layers.dense({units: 1, activation: 'linear'}));
+model.compile({optimizer: 'sgd', loss: 'meanSquaredError'});
+
+const xs = tf.randomNormal([100, 10]);
+const ys = tf.randomNormal([100, 1]);
+
+model.fit(xs, ys, {
+  epochs: 100,
+  callbacks: {
+    onEpochEnd: (epoch, log) => console.log(`Epoch ${epoch}: loss = ${log.loss}`)
+  }
+});
