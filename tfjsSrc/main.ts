f55b0259c1a614f5a91c14dc16ccb603b0ceb0b9
chenxuan <523764720@qq.com>
2020-07-22 17:57:09 +0800

    dev: 可以训练模型了

--- tfjsSrc/main.ts
+++ tfjsSrc/main.ts
@@ -2,17 +2,85 @@
 import * as tf from "@tensorflow/tfjs";
 
 // 训练一个简单模型:
-const model = tf.sequential();
-model.add(tf.layers.dense({units: 100, activation: 'relu', inputShape: [10]}));
-model.add(tf.layers.dense({units: 1, activation: 'linear'}));
-model.compile({optimizer: 'sgd', loss: 'meanSquaredError'});
+// const model = tf.sequential();
+// model.add(tf.layers.dense({units: 100, activation: 'relu', inputShape: [10]}));
+// model.add(tf.layers.dense({units: 1, activation: 'linear'}));
+// model.compile({optimizer: 'sgd', loss: 'meanSquaredError'});
 
-const xs = tf.randomNormal([100, 10]);
-const ys = tf.randomNormal([100, 1]);
+// const xs = tf.randomNormal([100, 10]);
+// const ys = tf.randomNormal([100, 1]);
 
-model.fit(xs, ys, {
-  epochs: 100,
-  callbacks: {
-    onEpochEnd: (epoch, log) => console.log(`Epoch ${epoch}: loss = ${log.loss}`)
-  }
-});
+// model.fit(xs, ys, {
+//   epochs: 100,
+//   callbacks: {
+//     onEpochEnd: (epoch, log) => console.log(`Epoch ${epoch}: loss = ${log.loss}`)
+//   }
+// });
+
+(async () =>
+{
+    // 创建模型 1维输入1维输出
+    let model = tf.sequential();
+
+    model.add(tf.layers.dense({inputShape: [1], units: 1}));
+    model.add(tf.layers.dense({units: 1}));
+
+    // 训练数据
+    let trainData = []
+    for(let i = 0; i != 10; ++i)
+    {
+        let horsepower = Math.round(Math.random() * 100)
+        let dataOne = {
+            "mpg":horsepower + 3,
+            "horsepower":horsepower,
+        }
+        trainData.push(dataOne)
+    }
+
+    tf.util.shuffle(trainData);
+    console.log(trainData);
+
+    const inputs = trainData.map(d => d.horsepower)
+    const labels = trainData.map(d => d.mpg)
+    console.log(inputs);
+    console.log(labels);
+
+    const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
+    const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
+
+    const inputMax = inputTensor.max();
+    const inputMin = inputTensor.min();  
+    const labelMax = labelTensor.max();
+    const labelMin = labelTensor.min();
+
+    const normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin));
+    const normalizedLabels = labelTensor.sub(labelMin).div(labelMax.sub(labelMin));
+
+    let ttdata = {
+        inputs: normalizedInputs,
+        labels: normalizedLabels,
+        inputMax,
+        inputMin,
+        labelMax,
+        labelMin,
+    }
+
+    // 准备模型
+    model.compile({
+        optimizer: tf.train.adam(),
+        loss: tf.losses.meanSquaredError,
+        metrics: ['mse'],
+    });
+
+    const batchSize = 32;
+    const epochs = 50;
+
+    
+    model.fit(normalizedInputs, normalizedLabels, {
+        batchSize: batchSize,
+        epochs: epochs,
+        callbacks: {
+            onEpochEnd: (epoch, log) => console.log(`Epoch ${epoch}: loss = ${log.loss}`)
+        }
+    });
+})()
